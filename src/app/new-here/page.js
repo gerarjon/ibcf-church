385e38a37bdeb2d32491c43b7ee448a8f1adad13
Gerar <gerarsuaverdez@gmail.com>
2024-09-07 14:13:18 -0700

    added 2nd section of page

--- src/app/new-here/page.js
+++ src/app/new-here/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function NewHere() {
   return (
-    <main className="pt-20">
-      <div className="container mx-auto px-5 m-5">
+    <main className="pt-20 px-5">
+      <div className="container mx-auto m-5">
         <div className="flex">
           <div className="basis-3/5 flex flex-col">
             <h3 className="text-lg lg:text-3xl text-subtitle font-black">New here?</h3>
@@ -14,6 +14,7 @@ export default function NewHere() {
             <div className="row-span-2">
               <img
                 src="https://res.cloudinary.com/ibcf/image/upload/v1723851932/IMG_0070_x3xp0q.jpg"
+                alt="Picture of pastor"
                 className="w-full h-full object-cover"
               />
             </div>
@@ -21,6 +22,7 @@ export default function NewHere() {
             <div>
               <img
                 src="https://res.cloudinary.com/ibcf/image/upload/v1723851707/IMG_0446_xlvpww.jpg"
+                alt="Picture of church playground"
                 className="w-full h-full object-cover"
               />
             </div>
@@ -33,9 +35,26 @@ export default function NewHere() {
               />
             </div>
           </div>
-        </div>
-        
+        </div>  
       </div>
+
+      <section className="container px-5 mx-auto mt-20">
+        <div className="flex">
+          <div className="basis-2/5 flex flex-col space-y-5 leading-relaxed">
+            <h3 className="flex flex-col text-4xl uppercase italic font-bold">Welcome <span>to</span> Our Church</h3>
+
+            <p>Whether you're visiting, exploring faith for the first  time, returning to church or looking for a place to call home, you're so welcome!</p>
+
+            <p>Connect with us outside of church too!</p>
+
+            <div className="display flex space-x-3"><a href="#">fb</a><a href="#">yt</a></div>
+          </div>
+          
+          <div>
+
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
